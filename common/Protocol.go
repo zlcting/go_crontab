fac9cda0e8ce6360df40b2b50d1eb87f4270215a
zlcting <zlcting@gmail.com>
2018-12-17 18:58:47 +0800

    111111

--- common/Protocol.go
+++ common/Protocol.go
@@ -1,7 +1,35 @@
 package common
 
+import (
+	"encoding/json"
+)
+
 type Job struct {
 	Name     string `json:"name"`
 	Command  string `json:"command"`
 	CronExpr string `json:"cronExpr"`
+}
+
+//http 接口应答
+
+type Response struct {
+	Errno int         `json:"errno"`
+	Msg   string      `json:"msg"`
+	data  interface{} `json:"data"`
+}
+
+//应答方法
+
+func BuildResponse(errno int, msg string, data interface{}) (resp []byte, err error) {
+	var (
+		response Response
+	)
+
+	response.Errno = errno
+	response.Msg = msg
+	response.data = data
+	//序列化
+	resp, err = json.Marshal(response)
+
+	return
 }
