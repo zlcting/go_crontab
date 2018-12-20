59705dd139434d96412bd955a8bb7532d4ffbca3
zlcting <zlcting@gmail.com>
2018-12-20 16:31:58 +0800

    111111

--- common/Protocol.go
+++ common/Protocol.go
@@ -2,20 +2,27 @@ package common
 
 import (
 	"encoding/json"
+	"strings"
 )
 
+// 定时任务
 type Job struct {
-	Name     string `json:"name"`
-	Command  string `json:"command"`
-	CronExpr string `json:"cronExpr"`
+	Name     string `json:"name"`     //  任务名
+	Command  string `json:"command"`  // shell命令
+	CronExpr string `json:"cronExpr"` // cron表达式
 }
 
-//http 接口应答
-
+// HTTP接口应答
 type Response struct {
 	Errno int         `json:"errno"`
 	Msg   string      `json:"msg"`
 	Data  interface{} `json:"data"`
+}
+
+// 变化事件
+type JobEvent struct {
+	EventType int //  SAVE, DELETE
+	Job       *Job
 }
 
 //应答方法
@@ -32,4 +39,30 @@ func BuildResponse(errno int, msg string, data interface{}) (resp []byte, err er
 	resp, err = json.Marshal(response)
 
 	return
+}
+
+//反序列job
+func UnpackJob(value []byte) (ret *Job, err error) {
+	var (
+		job *Job
+	)
+	job = &Job{}
+	if err = json.Unmarshal(value, job); err != nil {
+		return
+	}
+	ret = job
+	return
+}
+
+//从etcd的key中提取任务名称
+func ExtractJobName(jobKey string) string {
+	return strings.TrimPrefix(jobKey, JOB_SAVE_DIR)
+}
+
+// 任务变化事件有2种：1）更新任务 2）删除任务
+func BuildJobEvent(eventType int, job *Job) (jobEvent *JobEvent) {
+	return &JobEvent{
+		EventType: eventType,
+		Job:       job,
+	}
 }
