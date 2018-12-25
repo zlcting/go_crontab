79d11d98c2943063dc6c068e1a3cd9a1faee1051
zlcting <zlcting@gmail.com>
2018-12-25 17:30:58 +0800

    调度器

--- worker/Scheduler.go
+++ worker/Scheduler.go
@@ -1,6 +1,8 @@
 package worker
 
 import (
+	"fmt"
+	"time"
 	"zlc_sys/common"
 )
 
@@ -34,22 +36,68 @@ func (scheduler *Scheduler) handleJobEvent(jobEvent *common.JobEvent) {
 
 }
 
+//计算任务调度状态
+func (scheduler *Scheduler) TrySchedule() (schedulerAfter time.Duration) {
+	var (
+		jobPlan  *common.JobSchedulePlan
+		now      time.Time
+		nearTime *time.Time
+	)
+	//如果任务表为空的话，随便睡眠
+	if len(scheduler.jobPlanTable) == 0 {
+		schedulerAfter = 1 * time.Second
+		return
+	}
+	now = time.Now()
+
+	//遍历任务
+	for _, jobPlan = range scheduler.jobPlanTable {
+
+		if jobPlan.NextTime.Before(now) || jobPlan.NextTime.Equal(now) {
+			//TODO尝试执行任务
+			fmt.Println("执行任务", jobPlan.Job.Name)
+			jobPlan.NextTime = jobPlan.Expr.Next(now) //更新下次执行时间
+		}
+
+		//统计最近一个要过期的任务时间
+		if nearTime == nil || jobPlan.NextTime.Before(*nearTime) {
+			nearTime = &jobPlan.NextTime
+		}
+
+	}
+	//下次调度间隔 （最近要执行的任务调度时间-当前时间）
+	schedulerAfter = (*nearTime).Sub(now)
+
+	return
+}
+
 //调度协程
 func (scheduler *Scheduler) scheduleLoop() {
 	var (
-		jobEvent *common.JobEvent
-		// scheduleAfter time.Duration
-		// scheduleTimer *time.Timer
+		jobEvent      *common.JobEvent
+		scheduleAfter time.Duration
+		scheduleTimer *time.Timer
 		// jobResult     *common.JobExecuteResult
 	)
+	//初始化一次
+	scheduleAfter = scheduler.TrySchedule()
+	//调度的延时定时器
+	scheduleTimer = time.NewTimer(scheduleAfter)
 	//定时任务common.job
 	for {
 		select {
 		case jobEvent = <-scheduler.jobEventChan:
+			//对内存中维护的任务列表做增删改查
+			scheduler.handleJobEvent(jobEvent)
+		case <-scheduleTimer.C: //最近的任务到期了
 
 		}
+		//调度一次任务
+		scheduleAfter = scheduler.TrySchedule()
+		//重置调度间隔
+		scheduleTimer.Reset(scheduleAfter)
 	}
-	jobEvent = jobEvent
+
 }
 
 //推送任务变化事件
